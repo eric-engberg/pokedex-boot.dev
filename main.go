a6b02389b8e1b91ed8d1fb51cd1fbfac04a1be1c
eric-engberg <engberg.eric@gmail.com>
2026-03-15 14:03:02 -0500

    Add map command for locations

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"net/http"
+	"encoding/json"
 )
 
 type cliCommand struct {
@@ -12,8 +14,26 @@ type cliCommand struct {
 	callback func() error
 }
 
+type Location struct {
+	Count    int    `json:"count"`
+	Next     *string `json:"next"`
+	Previous *string    `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+type config struct {
+	Next *string
+	Previous *string
+}
+
 func main() {
+
+	baseURL := "https://pokeapi.co/api/v2/"
 	var commands map[string]cliCommand
+	var config config
 
 	commands = map[string]cliCommand{
 		"help": {
@@ -28,9 +48,24 @@ func main() {
 			description: "Exit the program",
 			callback: commandExit,
 		},
+		"map": {
+			name: "map",
+			description: "Get a list of locations",
+			callback: func() error {
+				return commandMap(&config, baseURL)
+			},
+		},
+		"mapb": {
+			name: "mapb",
+			description: "Get the previous page of locations",
+			callback: func() error {
+				return commandMapb(&config)
+			},
+		},
 	}
 
 	scanner := bufio.NewScanner(os.Stdin)
+
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
@@ -70,5 +105,85 @@ func commandHelp(commands map[string]cliCommand) error {
 func commandExit() error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
+	return nil
+}
+
+func commandMap(config *config, baseURL string) error {
+	url := baseURL + "/location-area"
+
+	if config.Next != nil {
+		url = *config.Next
+	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Accept", "application/json")
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	var locations Location
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&locations)
+
+	if err != nil {
+		return err
+	}
+
+	config.Next = locations.Next
+	config.Previous = locations.Previous
+
+	fmt.Println("Locations:")
+	for _, location := range locations.Results {
+		fmt.Println(location.Name)
+	}
+
+	return nil
+}
+
+func commandMapb(config *config) error {
+
+	if config.Previous == nil {
+		fmt.Println("you're on the first page")
+		return nil
+	}
+
+	url := *config.Previous
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Accept", "application/json")
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	var locations Location
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&locations)
+
+	if err != nil {
+		return err
+	}
+
+	config.Next = locations.Next
+	config.Previous = locations.Previous
+
+	fmt.Println("Locations:")
+	for _, location := range locations.Results {
+		fmt.Println(location.Name)
+	}
+
 	return nil
 }
